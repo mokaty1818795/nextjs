3d1d461956dd23efcf86bae7f0bec42d39719807
Tlotliso <tlotlisomokati@gmail.com>
2024-04-18 12:07:15 +0200

    added route segments and custome notfound page

---
+++ my-app/src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Docs() {
+  return (
+    <>
+      <h1>Docs</h1>
+    </>
+  );
+}
--- my-app/src/app/products/[productid]/reviews/[reviewid]/page.tsx
+++ my-app/src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -1,8 +1,14 @@
+import React from "react";
+import { notFound } from "next/navigation";
+
 export default function Review({
   params,
 }: {
   params: { productid: string; reviewid: string };
 }) {
+  if (parseInt(params.reviewid) > 100) {
+    return notFound();
+  }
   return (
     <>
       <h1>Product Review {params.reviewid}</h1>
